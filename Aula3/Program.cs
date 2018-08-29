e412d9f76b34de2121c7811809a0062c3da72549
jessels <jesse-silveira@hotmail.com>
2018-08-29 00:34:38 -0300

    Curso de C# Telecon - 28/08
    Curso de C# Telecon - 28/08

---
+++ Aula3/Program.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aula3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            /* Exercicio 1
+                        Console.WriteLine("digite a operação matemática");
+                        string operacaoMatematica = Console.ReadLine();
+
+                        switch (operacaoMatematica)
+                        {
+                            case "+":
+                                Console.WriteLine("Operação de Soma");
+                                break;
+                            case "-":
+                                Console.WriteLine("Operação de subtração");
+                                break;
+                            case "*":
+                                Console.WriteLine("Operação de multiplicação");
+                                break;
+                            case "/":
+                                Console.WriteLine("Operação de Divisão");
+                                break;
+                            default:
+                                Console.WriteLine("operação invalida");
+                                break;
+                        }*/
+            //Exercicio 2
+            /*
+            Console.WriteLine("Qual a sua aliquota de Imposto de Renda?");
+            var aliquotaImposto = decimal.Parse(Console.ReadLine());
+
+            switch(aliquotaImposto)
+                {
+                case 0:
+                    Console.WriteLine("Base de calculo é de R$1903,98");
+                    break;
+                case 7.5M:
+                    Console.WriteLine("Base de calculo é de R$1903,99 até 2826,65");
+                    break;
+                case 15M:
+                    Console.WriteLine("Base de calculo é de 2826,66 até 3751,05");
+                    break;
+                case 22.5M:
+                    Console.WriteLine("Base de calculo é de 3751,06 até 4664,68");
+                    break;
+                case 27.5M:
+                    Console.WriteLine("Base de calculo é acima de 4664,69");
+                    break;
+                default:
+                    Console.WriteLine("Invalido");
+                    break;
+
+            }*/
+            //Exercicio 3
+            /*
+            Console.WriteLine("Digite um valor Inteiro: ");
+            var numeroInteiro = int.Parse(Console.ReadLine());
+
+            while(numeroInteiro != 0)
+            {
+                Console.WriteLine(numeroInteiro--);
+            }*/
+
+            /*
+            var numero = 0;
+            while (numero <= 100)
+            {
+                Console.WriteLine("Continuar S ou N");
+                var interacao = Console.ReadLine();
+                if (interacao == "s")
+                {
+                    Console.WriteLine(numero++);
+                    continue;
+                }else
+                Console.WriteLine(numero++);
+                break;
+
+            }*/
+
+            Console.WriteLine("Quantas pessoas participaram da pesquisa");
+            var qtPesquisa = int.Parse(Console.ReadLine());
+            var idade = 0;
+            var idadeMedia = 0;
+            for(int i = 0; i< qtPesquisa ; i++)
+            {
+                Console.WriteLine("qual sua idade");
+                idade = int.Parse(Console.ReadLine());
+                idadeMedia = idade;
+                
+            }
+            
+            Console.WriteLine("");
+            Console.WriteLine(idadeMedia);
+
+
+
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Exercicio1/Program.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicio1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // Exercicio 1
+
+            /* Console.WriteLine("Digite seu nome: ");
+             var nome = Console.ReadLine();
+             Console.WriteLine("Digite o ano de nascimento: ");
+             var anoNascimento = int.Parse(Console.ReadLine());
+             var ano = 2018;
+             var idade = 0;
+
+
+             idade = ano - anoNascimento;
+
+             Console.WriteLine("Nome: "+ nome + " Idade é: " + idade);
+
+             Console.ReadKey();*/
+
+            //Exercicio 2
+
+            /* Console.WriteLine("Digite seu nome: ");
+             var nome = Console.ReadLine();
+
+             Console.WriteLine("Digite seu salario: ");
+             var salario = decimal.Parse(Console.ReadLine());
+
+             Console.WriteLine("Digite seu percentual de aumento: ");
+             var percentualAumento = decimal.Parse(Console.ReadLine());
+
+
+             Console.WriteLine(nome + " seu novo salario é: " + ((salario * percentualAumento) / 100 + salario));
+
+             Console.ReadKey();*/
+            //Exemplo
+            /*  Console.WriteLine("Qual sua idade? ");
+              var idade = short.Parse(Console.ReadLine());
+
+              if (idade == 18)
+              {
+                  Console.WriteLine("Idade igual a 18");
+              }
+              else
+              {
+                  Console.WriteLine("Idade não é igual a 18");
+
+              }*/
+
+            /*
+            Console.WriteLine("Qual sua idade? ");
+            var idade = short.Parse(Console.ReadLine());
+
+            Console.WriteLine("Quantidade de pessoas");
+            var pessoas = int.Parse(Console.ReadLine());
+
+            if(idade >= 18 || pessoas > 1)
+            {
+                Console.WriteLine("Pode entrar na balada");
+            }
+            else
+            {
+                Console.WriteLine("Não Pode entrar na balada");
+                 if(pessoas > 1)
+                 {
+                     Console.WriteLine("Pode entrar na balada");
+                 }
+                 else
+                 {
+                     Console.WriteLine("Não Pode entrar na balada");
+                 }*/
+
+            Console.WriteLine("Qual seu salario: ");
+            var salario = decimal.Parse(Console.ReadLine());
+            
+            
+            if (salario <= 1903.98M)
+            {
+                Console.WriteLine("sem dedução de imposto de renda");
+            }
+            
+            if(salario >= 1903.99M && salario <= 2826.65M)
+            {
+                Console.WriteLine("Aliquota 7.5%, Valor do imposto é de " + (salario * 7.5M)/100);
+            }
+            if(salario >= 2826.66M && salario <= 3751.05M)
+            {
+                Console.WriteLine("Aliquota 15% Valor do imposto é de " + (salario * 15M) / 100);
+            }
+            if(salario >= 3751.06M && salario <= 4664.68M)
+            {
+                Console.WriteLine("Aliquota 22.5% Valor do imposto é de " + (salario * 22.5M) / 100);
+            }
+            if(salario > 4664.68M)
+            {
+                Console.WriteLine("Aliquota 27.5% Valor do imposto é de " + (salario * 27.5M) / 100);
+            }
+            Console.ReadKey();
+
+            }
+        
+
+            
+
+
+
+
+
+
+
+
+
+
+        
+    }
+}
