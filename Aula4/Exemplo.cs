e19ddb889e3adea4367573042df189ec998cf54a
jessels <jesse-silveira@hotmail.com>
2018-09-11 19:04:54 -0300

    Exer 6

--- Aula4/Exemplo.cs
+++ Aula4/Exemplo.cs
@@ -48,6 +48,53 @@ namespace Aula4
 
             Console.WriteLine("Soma do salario "+ somaSalario);*/
 
+            //Exercicio 3
+            /*
+            Console.WriteLine("Quantas pessoas: ");
+            var qtPessoas = int.Parse(Console.ReadLine());
+
+            int[] idade = new int[qtPessoas];
+
+            for (int i = 0; i < idade.Length; i++)
+            {
+                Console.WriteLine("Qual a sua idade? ");
+                idade[i] = int.Parse(Console.ReadLine());
+
+            }
+            for (int i = 0; i < idade.Length; i++)
+            {
+                if (idade[i] >= 18)
+                    Console.WriteLine("Idades da Lista "+ idade[i]);
+            
+        }*/
+
+            //Exercicio 4
+            /*
+            Console.WriteLine("Quantas pessoas: ");
+            var qtPessoas = int.Parse(Console.ReadLine());
+
+            string[] pessoas = new string[qtPessoas];
+            string[] topCinco = new string[5];
+            
+
+            for (int i = 0; i < pessoas.GetLength(0); i++)
+            {
+                Console.WriteLine("nome da pessoa");
+                pessoas[i] = Console.ReadLine();
+            }
+            for (int i = 0; i < 5; i++)
+            {
+                topCinco[i] = pessoas[i];
+
+            }
+            for (int i = 0; i < topCinco.Length; i++)
+            {
+                Console.WriteLine(topCinco[i]);
+
+            }*/
+
+
+            /* exercicio 5
             const int nome = 0;
             const int cidade = 1;
             const int bairro = 2;
@@ -75,11 +122,36 @@ namespace Aula4
                 Console.WriteLine("Cidade: " + clientes[i, cidade]);
                 Console.WriteLine("Bairro: " + clientes[i, bairro]);
                 Console.WriteLine("Estado: " + clientes[i, estado]);
+            }*/
+
+            //Exercio 6
+
+            const int nome = 0;
+            const int idade = 1;
+            int menorIdade = 0;
+            string[,] pessoas = new string[10, 2];
+
+            for (int i = 0; i < pessoas.GetLength(0); i++)
+            {
+                Console.WriteLine("Digite o nome da pessoa: ");
+                pessoas[i, nome] = Console.ReadLine();
+                Console.WriteLine("Digite a idade da pessoa: ");
+                pessoas[i, idade] = Console.ReadLine();
+
+                if (Convert.ToInt32(pessoas[i, idade]) < menorIdade || i == 0)
+                {
+                    menorIdade = Convert.ToInt32(pessoas[i, idade]);
+                }
             }
-            
-         
+            for (int i = 0; i < pessoas.GetLength(0); i++)
+            {
+                if (Convert.ToInt32(pessoas[i, idade]) < 18)
+                {
+                    Console.WriteLine(pessoas[i, nome] + " é menor de 18 anos!");
+                }
+            }
 
-
+            Console.WriteLine("A menor idade encontrada é :" + menorIdade);
             Console.ReadKey();
         }
     }
