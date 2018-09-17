a36e5af339baafa22316342b1b84981991d61492
jessels <jesse-silveira@hotmail.com>
2018-09-17 20:42:10 -0300

    Trabalho

--- Trabalho/Program.cs
+++ Trabalho/Program.cs
@@ -26,12 +26,12 @@ namespace Trabalho
             const int valor = 2;
             int codigoConstante = 0;
             int alteraproduto = 0;
-
-            int qtProdutos = 1;
-
-            string[,] produtos = new string[qtProdutos, 3];
+            int qtProdutos = 0;
 
 
+
+            
+            string[,] produtos = new string[1000, 3];
 
 
             string opcao = "0";
@@ -39,11 +39,9 @@ namespace Trabalho
             while (opcao != "")
             {
                 
-
-                Console.WriteLine("Selecione uma opção para realizar: 1 - Incluir, 2 - Alterar, 3 - Consultar, 4 - Imprimir todos ,  0 - Sair ");
+                Console.WriteLine("Selecione uma opção para realizar: \n 1 - Incluir \n 2 - Alterar \n 3 - Consultar \n 4 - Imprimir todos \n 0 - Sair ");
                 opcao = Console.ReadLine();
                 
-
                 switch (opcao)
                 {
                      
@@ -52,8 +50,8 @@ namespace Trabalho
                             Console.Clear();
                             
                             Console.WriteLine("Você está inserindo um produto");
-
-                            for (int i = 0; i < produtos.GetLength(0); i++)
+                        
+                            for (int i = 0; i < qtProdutos; i++)
                             {
                                 Console.WriteLine("Informe o Codigo do Produto: ");
                                 produtos[i, codigo] = Console.ReadLine();
@@ -68,7 +66,7 @@ namespace Trabalho
                                 }
                             
                             }
-                        
+                        qtProdutos++;
                         Console.WriteLine("Digite qualquer tecla para voltar: ");
                         opcao = Console.ReadLine();
                         Console.Clear();
@@ -122,7 +120,7 @@ namespace Trabalho
                         break;
                     case "4":
                         Console.Clear();
-                        for (int i = 0; i < produtos.GetLength(0); i++)
+                        for (int i = 0; i < qtProdutos; i++)
                         {
                             Console.WriteLine("Codigo: " + produtos[i, codigo] + " | " +
                                               " Descrição: " + produtos[i, descricao] + " | " +
